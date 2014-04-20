badf2830bbff55394d2623aae355b7bd5283b6b3
alfaiataria <rui.alfaiataria@gmail.com>
2014-04-20 19:12:30 +0100

    fixed

--- assets/js/layoutcomplete.js
+++ assets/js/layoutcomplete.js
@@ -1,76 +1,79 @@
-var $notifElem;
+$('.isotope').imagesLoaded(function() {
 
-$( function() {
+        var $notifElem;
 
-  var $container = $('.isotope').isotope({
-    itemSelector: '.item',
-    masonry: {
-      columnWidth: 100
-    }
-  });
+        $( function() {
 
-  $notifElem = $('#notification');
+          var $container = $('.isotope').isotope({
+            itemSelector: '.item',
+            masonry: {
+              columnWidth: 100
+            }
+          });
+
+          $notifElem = $('#notification');
+          
+          $container.isotope( 'on', 'layoutComplete', function( isoInstance, laidOutItems ) {
+            notify( 'Isotope layout completed with ' + laidOutItems.length + ' items' );
+          });
+          
+          $container.on( 'click', '.item', function() {
+            // change size of item by toggling gigante class
+            $( this ).toggleClass('gigante');
+            $container.isotope('layout');
+          });
+          
+        });
   
-  $container.isotope( 'on', 'layoutComplete', function( isoInstance, laidOutItems ) {
-    notify( 'Isotope layout completed with ' + laidOutItems.length + ' items' );
-  });
-  
-  $container.on( 'click', '.item', function() {
-    // change size of item by toggling gigante class
-    $( this ).toggleClass('gigante');
-    $container.isotope('layout');
-  });
-  
+
+
+        // --------- timestamp ----------- //
+
+        function timeStamp() {
+          var now = new Date();
+          var min = now.getMinutes();
+          min = min < 10 ? '0' + min : min;
+          var seconds = now.getSeconds();
+          seconds = seconds < 10 ? '0' + seconds : seconds;
+          return [ now.getHours(), min, seconds ].join(':');
+        }
+
+        // -------------------------- notify -------------------------- //
+
+        var transitionProp = getStyleProperty('transition');
+
+        var notifyTimeout;
+        var hideTime = transitionProp ? 1000 : 1500;
+
+        function notify( message ) {
+          message += ' at ' + timeStamp();
+          $notifElem.text( message );
+
+          var style = {};
+          
+          if ( transitionProp ) {
+            style[ transitionProp ] = 'none';
+          }
+          style.display = 'block';
+          style.opacity = 1;
+          $notifElem.css( style );
+
+          // hide the notification after a second
+          if ( notifyTimeout ) {
+            clearTimeout( notifyTimeout );
+          }
+
+          notifyTimeout = setTimeout( hideNotify, hideTime );
+        }
+
+        function hideNotify() {
+          var style = {};
+          if ( transitionProp ) {
+            style[ transitionProp ] = 'opacity 1.0s';
+            style.opacity = '0';
+          } else {
+            style.display = 'none';
+          }
+          $notifElem.css( style );
+        }
 });
-
-
-// --------- timestamp ----------- //
-
-function timeStamp() {
-  var now = new Date();
-  var min = now.getMinutes();
-  min = min < 10 ? '0' + min : min;
-  var seconds = now.getSeconds();
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-  return [ now.getHours(), min, seconds ].join(':');
-}
-
-// -------------------------- notify -------------------------- //
-
-var transitionProp = getStyleProperty('transition');
-
-var notifyTimeout;
-var hideTime = transitionProp ? 1000 : 1500;
-
-function notify( message ) {
-  message += ' at ' + timeStamp();
-  $notifElem.text( message );
-
-  var style = {};
-  
-  if ( transitionProp ) {
-    style[ transitionProp ] = 'none';
-  }
-  style.display = 'block';
-  style.opacity = 1;
-  $notifElem.css( style );
-
-  // hide the notification after a second
-  if ( notifyTimeout ) {
-    clearTimeout( notifyTimeout );
-  }
-
-  notifyTimeout = setTimeout( hideNotify, hideTime );
-}
-
-function hideNotify() {
-  var style = {};
-  if ( transitionProp ) {
-    style[ transitionProp ] = 'opacity 1.0s';
-    style.opacity = '0';
-  } else {
-    style.display = 'none';
-  }
-  $notifElem.css( style );
-}
-
